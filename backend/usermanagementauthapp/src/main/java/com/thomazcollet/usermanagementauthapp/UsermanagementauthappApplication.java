077e4033ada6594eaf23d5dc09cf44d075fafe84
Thomaz de aquino collet e silva <tcollet98@hotmail.com>
2026-08-18 17:11:30 -0300

    initial commit

---
+++ backend/usermanagementauthapp/src/main/java/com/thomazcollet/usermanagementauthapp/UsermanagementauthappApplication.java
@@ -0,0 +1,13 @@
+package com.thomazcollet.usermanagementauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementauthappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementauthappApplication.class, args);
+	}
+
+}
