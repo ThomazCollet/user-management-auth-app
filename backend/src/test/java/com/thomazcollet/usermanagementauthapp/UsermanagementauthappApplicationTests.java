a3a786679b975a2b34143e37bc2f0f4f7d185257
Thomaz de aquino collet e silva <tcollet98@hotmail.com>
2026-09-09 16:13:03 -0300

    test(user): implement UserController tests and restructure backend directories

--- backend/src/test/java/com/thomazcollet/usermanagementauthapp/UsermanagementauthappApplicationTests.java
+++ backend/src/test/java/com/thomazcollet/usermanagementauthapp/UsermanagementauthappApplicationTests.java
@@ -3,7 +3,11 @@ package com.thomazcollet.usermanagementauthapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.flyway.enabled=false",
+		"spring.jpa.hibernate.ddl-auto=none",
+		"spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect"
+})
 class UsermanagementauthappApplicationTests {
 
 	@Test
